971ef41636e91a050d83b494c86534c8ebceff54
bchaindeveloper <bcdev3@protonmail.com>
2022-09-13 14:48:33 -0400

    First Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,41 @@
+// imports
+const { ethers, run, network } = require("hardhat")
+
+//async main
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Deployed contact to: ${simpleStorage.address}`)
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+      await simpleStorage.deployTransaction.wait(6) 
+      await verify(simpleStorage.address, [])
+    }
+  }
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+    await run("verify:verify", {
+        address: contractAddress,
+        constructorArguments: args,
+    })
+    } catch (e) {
+      if (error.message.toLowerCase().includes("already verified")){
+        console.log("Already Verified")
+      } else {
+        console.log(e)
+      }
+    }
+}
+
+// main
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
